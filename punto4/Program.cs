42293d2473ded92214b1dec7721c935193e7f2cf
EmmanuelNBilkis <emmanuelbilkis@gmail.com>
2022-09-24 10:37:31 -0300

    punto 4 terminado

---
+++ punto4/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.IO;
+
+namespace Name
+{
+    class Program
+    {
+        static void main()
+        {
+            FuncionLLamadoraA();
+
+            try
+            {
+                FuncionLLamadoraB();
+            }
+            catch (System.DivideByZeroException e)
+            {
+                Console.WriteLine("La funcion b divide en 0" + e.Message);
+            }
+           
+
+            void FuncionLLamadoraA()
+            {
+                Dividir(3,3);
+            }
+
+            void FuncionLLamadoraB()
+            {
+                Dividir(3,0);
+            }
+
+            void Dividir(int a,int b)
+            {
+                try
+                {
+                    int c = a/b;
+                }
+                catch (System.DivideByZeroException e)
+                {
+                    Console.WriteLine("Divide en 0" + e.Message);
+                }
+                catch(System.FormatException e)
+                {
+                     Console.WriteLine("Error de formato ingresado" + e.Message);
+                }
+            }
+        }
+
+     
+    }
+}
+
+
